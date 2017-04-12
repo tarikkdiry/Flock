259c3dec1d81f64c7ee5982ef76d3b6f016ee269
Tarik Kdiry <tarik.kdiry@gmail.com>
2017-04-12 18:30:40 -0400

    part 6

--- app/models.py
+++ app/models.py
@@ -1,15 +1,20 @@
 from app import db
-
+from hashlib import md5
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
+    
     @property
     def is_authenticated(self):
         return True
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
 
     @property
     def is_active(self):
