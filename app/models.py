541ebff56434e80353bb460a02a0d025cbeb4960
Oscar Tavara <xoscart@gmail.com>
2017-04-12 17:10:44 -0400

    Part 5 done

--- app/models.py
+++ app/models.py
@@ -7,6 +7,24 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
