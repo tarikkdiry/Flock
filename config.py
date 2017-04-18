908fb5d249d0b26c78fad1c8d8c781d0c3f47aee
Tarik Kdiry <tarik.kdiry@gmail.com>
2017-04-17 23:26:56 -0400

    added profile page, login works now

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-CSRF_ENABLED = True
+WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id/<username>'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com/<username>'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'myStevens', 'url': 'http://mystevens.stevens.edu/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
